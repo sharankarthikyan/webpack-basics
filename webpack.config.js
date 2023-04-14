eacc6cc305f13d154be14c8cdabe1f7038b82b4a
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-14 10:06:40 +0530

    we have taught webpack, how to load css (css-loader) and how to apply style (style-loader) to html, those loaders are come as npm package and we added it in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         test: /\.txt$/,
         type: "asset/source",
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
