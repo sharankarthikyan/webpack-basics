fd554787925646039ed1063bb189971cb57692ef
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-14 08:55:12 +0530

    added nature image and not added any config in config file. It will cause an error.

--- src/add-image.js
+++ src/add-image.js
@@ -1,8 +1,10 @@
+import Nature from "./nature.jpeg";
+
 function addImage() {
   const img = document.createElement("img");
   img.alt = "nature";
   img.width = "300px";
-  img.src = "./nature.jpeg";
+  img.src = Nature;
 
   const body = document.querySelector("body");
   body.appendChild(img);
