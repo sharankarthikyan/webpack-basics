18bdaa04bfe3b22ffc04ea6eef1e4aba72b76310
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-15 18:47:43 +0530

    added prod and dev config for webpack - removed contenthash in dev, because it takes long time and lot of things changes

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ heading.render();
 
 const hwb = new HelloWorldButton();
 hwb.render();
+
+if (process.env.NODE_ENV === "production") {
+  console.log("Production mode");
+} else if (process.env.NODE_ENV === "development") {
+  console.log("Development mode");
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 1000 * 1024, // 1000 kilobytes
+          },
+        },
+      },
+      {
+        test: /\.txt$/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // This helps to clean the dist folder, because it is specified in output.path
+    new HtmlWebpackPlugin({
+      title: "Hello World",
+      template: "src/index.hbs",
+      description: "Some Description",
+    }), // This will create a new index.html with newly updated js and css file [contenthash].
+  ],
+};
