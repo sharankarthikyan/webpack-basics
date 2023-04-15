0e028fd4946c73f39bbf0873f5d8ff3c132c7e94
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-15 07:57:41 +0530

    MiniCssExtractPlugin to add seperate file for css - plugin

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
+import Heading from "./components/heading/heading";
+
+const heading = new Heading();
+heading.render();
 
 const hwb = new HelloWorldButton();
 hwb.render();
