ed885030979d94f13f7f68854d391f53087795a5
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-15 20:53:26 +0530

    created multi page app in express - completed

--- src/server.js
+++ src/server.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "../dist")));
+app.get("/hello-world/", (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/hello-world.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
+});
+
+app.get("/nature/", (req, res) => {
+  const pathToHtmlFile = path.resolve(__dirname, "../dist/nature.html");
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, "utf-8");
+  res.send(contentFromHtmlFile);
+});
+
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
