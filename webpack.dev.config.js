0c9fe918f35f12f0fdd094d9354ceab5dd734273
sharankarthikeyan <karthikeyansharan00@gmail.com>
2023-04-15 20:30:15 +0530

    changed dev config file

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    "nature": "./src/nature.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "",
   },
@@ -63,9 +66,20 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(), // This helps to clean the dist folder, because it is specified in output.path
     new HtmlWebpackPlugin({
+      filename: "hello-world.html",
+      chunks: ["hello-world"],
       title: "Hello World",
-      template: "src/index.hbs",
-      description: "Some Description",
-    }), // This will create a new index.html with newly updated js and css file [contenthash].
+      template: "src/page-template.hbs",
+      description: "Hello World Description",
+      minify: false,
+    }), // This will create a new hello-world.html with newly updated js and css file [contenthash].
+    new HtmlWebpackPlugin({
+      filename: "nature.html",
+      chunks: ["nature"],
+      title: "Nature",
+      template: "src/page-template.hbs",
+      description: "Nature Description",
+      minify: false,
+    }), // This will create a new nature.html with newly updated js and css file [contenthash].
   ],
 };
